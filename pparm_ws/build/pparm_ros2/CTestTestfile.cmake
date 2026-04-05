3ff7a5e0bb38c99bd70a68ed21094bf77cc78d88
telegdi <telegdics34@gmail.com>
2026-04-05 12:35:44 +0000

    módosítások a world.launch elkészítése végett

---
+++ pparm_ws/build/pparm_ros2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/telegdicsongor/Robot_projekt/pparm_ws/src/pparm_ros2
+# Build directory: /home/telegdicsongor/Robot_projekt/pparm_ws/build/pparm_ros2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
